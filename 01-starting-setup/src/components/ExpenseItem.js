316d16e33b640854bcdbc1e2f2f1fb7f45055e37
abhishek1886 <guptaabhishek1886@gmail.com>
2023-07-12 16:46:48 +0530

    Reattask7

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h3>Expense Item</h3>
+      <li>Food Rs 10</li>
+      <li>Petrol Rs 100</li>
+      <li>Movies Rs 200</li>
+    </div>
+  );
+}
+
+export default ExpenseItem;
