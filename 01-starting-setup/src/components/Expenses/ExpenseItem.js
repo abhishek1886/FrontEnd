8305244f7a121b8ecc0a06d227e9c5f519ef8b2e
abhishek1886 <guptaabhishek1886@gmail.com>
2023-07-13 14:55:34 +0530

    AdReactTask02

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,53 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
-import Card from "../UI/Card";  
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const clickHandler = () => {
-    //code to delete the expense
+
+  const [isVisible, setIsVisible] = useState(true);
+  const [title, setTitle] = useState(props.title);
+  const [amount, addAmount] = useState(props.amount)
+
+  //Add $100 to current expense
+  const clickHandlerAddAmount = () => {
+    addAmount(amount+100);
   }
 
+  //delete the current expense
+  const clickHandlerDelete = () => {
+    setIsVisible(false);
+  };
+
+  //add a title
+  const clickHandlerEdit = () => {
+    setTitle('Updated!')
+  };
+
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <ExpenseDetails
-        amount={props.amount}
-        title={props.title}
-        location = {props.location}
-      />
-      <button onClick = {clickHandler}>Delete</button>
-    </Card>
+    isVisible && (
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <ExpenseDetails
+          amount={amount}
+          title={title}
+          location={props.location}
+        />
+        <div>
+          <button style={{background: 'green'}} className="button-item" onClick={clickHandlerAddAmount}>
+            +$100
+          </button>
+          <button style={{background: 'red'}} className="button-item" onClick={clickHandlerDelete}>
+            Delete
+          </button>
+          <button style={{background: 'blue'}} className="button-item" onClick={clickHandlerEdit}>
+            Edit Title
+          </button>
+        </div>
+      </Card>
+    )
   );
 }
 
