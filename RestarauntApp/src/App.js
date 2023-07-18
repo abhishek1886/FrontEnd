e7a2e06cb665d1a6261e8855c960f2c73ca83a6d
abhishek1886 <guptaabhishek1886@gmail.com>
2023-07-18 22:59:40 +0530

    added the meals summary

--- RestarauntApp/src/App.js
+++ RestarauntApp/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Header from "./Components/Layout/Header";
+import MealsSummary from "./Components/Meals/MealsSummary";
 
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
+      <MealsSummary />
     </React.Fragment>
   );
 }
