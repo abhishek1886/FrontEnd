bbd746b9167148f1fe6f37349b2a8144a46fae91
abhishek1886 <guptaabhishek1886@gmail.com>
2023-07-18 23:15:35 +0530

    making the items visible

--- RestarauntApp/src/App.js
+++ RestarauntApp/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "./Components/Layout/Header";
 import MealsSummary from "./Components/Meals/MealsSummary";
+import AvailableMeals from "./Components/Meals/AvailableMeals";
 
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
       <MealsSummary />
+      <AvailableMeals />
     </React.Fragment>
   );
 }
--- RestarauntApp/src/Components/Meals/AvailableMeals.js
+++ RestarauntApp/src/Components/Meals/AvailableMeals.js
@@ -1,5 +1,50 @@
+import classes from './AvailableMeals.module.css'
+
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
+    price: 12.99,
+  },
+  {
+    id: 'm4',
+    name: 'Green Bowl',
+    description: 'Healthy...and green...',
+    price: 18.99,
+  },
+];
+
+
+
 const AvailableMeals = () => {
-  
+
+  return <div className={classes.meals}>
+    <ul>
+      {DUMMY_MEALS.map(meal => {
+        return (
+          <li key={meal.id}>
+            <p>{meal.name}</p>
+            <p>{meal.description}</p>
+            <p>${meal.price}</p>
+            <hr />
+          </li>
+        )
+      })}
+    </ul>
+  </div>
 }
 
 export default AvailableMeals;
