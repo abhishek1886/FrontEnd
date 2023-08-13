cc903a436a38a9ebb994f776214c3b589e575848
abhishek1886 <guptaabhishek1886@gmail.com>
2023-08-13 15:44:23 +0530

    added Links

--- nextjs/nex-app/pages/aboutus/[developer].js
+++ nextjs/nex-app/pages/aboutus/[developer].js
@@ -1,14 +1,10 @@
 import { useRouter } from "next/router";
 
 const details = [
-
   { id : 1, name: 'Yash', role: 'Senior Developer'},
-  
   { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
-  
   { id : 3, name: 'Suresh', role: 'Frontend Developer'}
-  
-  ]
+]
 
 const DeveloperPage = () => {
   const router = useRouter();
--- nextjs/nex-app/pages/aboutus/index.js
+++ nextjs/nex-app/pages/aboutus/index.js
@@ -1,5 +1,23 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
+const details = [
+  { id : 1, name: 'Yash', role: 'Senior Developer'},
+  { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
+  { id : 3, name: 'Suresh', role: 'Frontend Developer'}
+]
 const NewsPage = () => {
-  return <h1>About Us page.</h1>;
+  const userData = details.map(data => (
+    <li key={data.id}><Link href={`/aboutus/${data.id}`}>{data.name}</Link></li>
+  ))
+  return (
+    <Fragment>
+      <h1>About Us page.</h1>
+      <ul>
+        {userData}
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
